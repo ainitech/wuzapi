b714968c704957a98fe887d6f0bca700de49abae
AINI TECH <72950126+ainitech@users.noreply.github.com>
2025-07-24 16:40:19 -0300

    Update main.go

--- main.go
+++ main.go
@@ -107,11 +107,12 @@ func main() {
 		fmt.Println(err)
 	}
 
+	ctx := context.Background()
 	if *waDebug != "" {
 		dbLog := waLog.Stdout("Database", *waDebug, true)
-		container, err = sqlstore.New("sqlite", "file:"+exPath+"/dbdata/main.db?_pragma=foreign_keys(1)&_busy_timeout=3000", dbLog)
+		container, err = sqlstore.New(ctx, "sqlite", "file:"+exPath+"/dbdata/main.db?_pragma=foreign_keys(1)&_busy_timeout=3000", dbLog)
 	} else {
-		container, err = sqlstore.New("sqlite", "file:"+exPath+"/dbdata/main.db?_pragma=foreign_keys(1)&_busy_timeout=3000", nil)
+		container, err = sqlstore.New(ctx, "sqlite", "file:"+exPath+"/dbdata/main.db?_pragma=foreign_keys(1)&_busy_timeout=3000", nil)
 	}
 	if err != nil {
 		panic(err)
